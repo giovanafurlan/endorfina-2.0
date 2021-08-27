cb0beddad5653c7f44057696877351cfb7e50254
giovanafurlan <RM88936@fiap.com.br>
2021-08-27 11:16:42 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/header/Header.jsx'
+import Corpo from './components/corpo/Corpo.jsx'
+
+export default function App() {
+
+   return(
+    <div className= "container">
+      <Header/>
+      <Corpo/>
+    </div>
+)}
