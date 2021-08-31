d07aaf91335d71526404e27ff8b94dd464c5df50
giovanafurlan <RM88936@fiap.com.br>
2021-08-31 11:38:06 -0300

    Adicionando depoimentos

--- src/components/corpo/Corpo.jsx
+++ src/components/corpo/Corpo.jsx
@@ -3,8 +3,10 @@ import fundo from '../../img/fundo.png'
 import fundo2 from '../../img/fundo2.png'
 import fundo3 from '../../img/fundo3.png'
 import hobbies from '../../img/hobbies.png'
-import esportes from '../../img/esportes.jpg'
 import Header from '../header/Header'
+import user1 from '../../img/user1.png'
+import user2 from '../../img/user2.png'
+import user3 from '../../img/user3.png'
 
 import './Corpo.css'
 
@@ -13,8 +15,6 @@ export default function Corpo() {
    return (
       <>
          <section>
-            {/* <h1><img src= {esportes}/></h1> */}
-
             <div className="texto">
                <h2 className= "titulo">Endorfina</h2>
                <p> <img src= {fundo} align="left"/><br></br>
@@ -22,13 +22,34 @@ export default function Corpo() {
                <br></br>A missão da plataforma é obter uma gama de opções de hobbies para enriquecer o projeto cada vez mais, a visão é fornecer um bom-estar ao usuário, um ambiente seguro, que seja uma válvula de escape de suas tensões que vem a ocasiaonar uma crise de ansiedade.
                </p>
             </div>
-
+            
             <div className="texto">
                <h2 className= "ansiedade">Ansiedade</h2>
                <p> <img src= {fundo2} align="left"/><br></br>
                Segundo artigo do site Dasa "A ansiedade na quarentena tem sido uma grande preocupação desde o início da quarentena em meio à pandemia do coronavírus. Desde que o isolamento social foi implementado, como medida preventiva contra a nova doença, tivemos que nos adaptar a novos comportamentos e rotinas. Diante de tantas mudanças, é normal que o nosso corpo e nossa mente sintam esse impacto."<br></br>
                <br></br>"Segundo dados da Organização Mundial da Saúde (OMS), o Brasil tem o maior número de pessoas ansiosas do mundo: 18,6 milhões de brasileiros (9,3% da população) convivem com o transtorno. Com a pandemia, esses sintomas podem ser evidenciados, por isso é importante aprender como lidar com seus efeitos."
                </p>
+            </div>
+
+            <h2 className= "depoimentos">Depoimentos</h2>
+            <div className="box">
+               <figure>
+                  <img src= {user1}/>
+                  <figcaption className= "nome">Maria</figcaption>
+                  <figcaption>"A plataforma é incrível, me ajudou demais a me distrair das crises de ansiedade, e também descobrir um dom que tenho na pintura"</figcaption>
+               </figure>
+
+               <figure>
+                  <img src= {user2}/>
+                  <figcaption className= "nome">Lara</figcaption>
+                  <figcaption>"A plataforma é incrível, me ajudou demais a me distrair das crises de ansiedade, e também descobrir um dom que tenho na pintura"</figcaption>
+               </figure>
+
+               <figure>
+                  <img src= {user3}/>
+                  <figcaption className= "nome">Paulo</figcaption>
+                  <figcaption>"A plataforma é incrível, me ajudou demais a me distrair das crises de ansiedade, e também descobrir um dom que tenho na pintura"</figcaption>
+               </figure>
             </div>
 
             <div className="texto">
